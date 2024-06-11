57ed025b9f41f48dccb4318e7c0832690f7c707a
Dmytro Dzhemchuk <dzhemchuk@gmail.com>
2024-06-11 12:11:33 -0400

    Router setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import PortfolioPage from "./pages/PortfolioPage";
+import ContactPage from "./pages/ContactPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "portfolio", element: <PortfolioPage /> },
+      { path: "contact", element: <ContactPage /> },
+    ],
+  },
+]);
+
 function App() {
-  return;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
